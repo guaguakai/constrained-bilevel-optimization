e8d78fab6b1745143440ebf4e103305c5adf44e9
guaguakai <kwang692@gatech.edu>
2024-05-03 20:41:29 +0800

    added best response, added scripts, added results folder

--- main.py
+++ main.py
@@ -17,24 +17,28 @@ from cvxpylayers.torch import CvxpyLayer
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--solver', default = 'ffo', choices=['ffo', 'cvxpylayer'])
+    parser.add_argument('--solver', default = 'ffo', choices=['ffo', 'BR', 'cvxpylayer'])
     parser.add_argument('--lr', default = 1e-3, type = float)
+    parser.add_argument('--xdim', default = 100, type = int)
+    parser.add_argument('--ydim', default = 500, type = int)
+    parser.add_argument('--n-constraints', default = 100, type = int)
     parser.add_argument('--seed', default = 0, type = int)
     parser.add_argument('--steps', type=int, default=20)
     parser.add_argument('--n-iterations', type=int, default=1000)
 
     args = parser.parse_args()
     
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
+    seed = args.seed
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
     lr = args.lr
     solver = args.solver
 
     # Parameters
-    x_dim = 200
-    y_dim = 500
-    n_constraints = 100
+    x_dim = args.xdim
+    y_dim = args.ydim
+    n_constraints = args.n_constraints
 
     c = torch.rand(y_dim)
     L = torch.rand(y_dim, y_dim)
@@ -51,20 +55,22 @@ if __name__ == '__main__':
     b_cp = b.numpy()
     
     # Define functions
-    f = lambda x,y: c @ y 
+    f = lambda x,y: c @ y + 0.01 * torch.norm(x) + 0.01 * torch.norm(y)
     g = lambda x,y: 1/2 * y.t() @ Q @ y + x.t() @ P @ y
     h = lambda x,y: A @ y - b 
-
-    # Define lower level problem in CVXPY
-    # TODO
 
     # Compute gradient using pytorch
     x = torch.rand(x_dim, requires_grad=True) # random initialization
     optimizer = torch.optim.Adam([x], lr=lr)
 
+    # Output file
+    f_output = open('results/ydim{}/{}_seed{}.txt'.format(ydim, solver, seed), 'w')
+    f_output.write('Iteration, loss, time \n')
+
     if solver == 'cvxpylayer':
         # Cvxpy (differentiable optimization) approach
         for i in range(args.n_iterations):
+            start_time = time.time()
             x_cp = cp.Parameter(x_dim)
             y_cp = cp.Variable(y_dim)
             q_cp = x_cp.T @ P_cp
@@ -77,13 +83,35 @@ if __name__ == '__main__':
             loss = f(x, solution)  # + 1/2 * x @ x
             loss.backward()
             optimizer.step()
-            print(i, loss.item())
+            time_elapsed = time.time() - start_time
+            print(i, loss.item(), time_elapsed)
+            f_output.write('{}, {}, {} \n'.format(i, loss, time_elapsed))
+
+    elif solver == 'BR':
+        for i in range(args.n_iterations):
+            start_time = time.time()
+            x_cp = x.detach().numpy()
+            y_cp = cp.Variable(y_dim)
+            q_cp = x_cp.T @ P_cp
+            objective = cp.Minimize(0.5 * cp.quad_form(y_cp, Q_cp) + q_cp @ y_cp)
+            constraints = [A @ y_cp - b <= 0]
+            problem = cp.Problem(objective, constraints)
+            problem.solve()
+    
+            loss = f(x, y_cp.value)  # + 1/2 * x @ x
+            loss.backward()
+            optimizer.step()
+
+            time_elapsed = time.time() - start_time
+            print(i, loss.item(), time_elapsed)
+            f_output.write('{}, {}, {} \n'.format(i, loss, time_elapsed))
 
     elif solver == 'ffo':
         # Our algorithm
         lamb_init = 1
         lamb = lamb_init
         for i in range(args.n_iterations):
+            start_time = time.time()
             # Pre-solve inner optimization problem:  min_y max_\gamma g(x,y) + \gamma^\top h(x,y)
             x_cp = x.detach().numpy()
             y_cp = cp.Variable(y_dim)
@@ -122,7 +150,13 @@ if __name__ == '__main__':
             final_lagrangian.backward()
             optimizer.step()
             loss = f(x, y_opt).item()
-            print(i, loss) 
 
             # lambda update # TODO
             lamb = lamb_init
+
+            # Output file
+            time_elapsed = time.time() - start_time
+            print(i, loss, time_elapsed)
+            f_output.write('{}, {}, {} \n'.format(i, loss, time_elapsed))
+
+    f_output.close()
