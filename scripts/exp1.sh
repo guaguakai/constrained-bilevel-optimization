c021384781f181c43ae841a3ac7c4342feab2b1e
guaguakai <kwang692@gatech.edu>
2024-05-11 06:36:15 +0800

    updated exp setup

--- scripts/exp1.sh
+++ scripts/exp1.sh
@@ -1,4 +1,6 @@
 FOLDER=exp1
+EPS=0.01
+ITER=1000
 
 for SEED in 1  # {1..5}
 do
@@ -7,7 +9,7 @@ do
 		NCON=$((YDIM/5))
 		for SOLVER in ffo # cvxpylayer ffo
 		do
-			sbatch --export=FOLDER=$FOLDER,SOLVER=$SOLVER,SEED=$SEED,YDIM=$YDIM,NCON=$NCON scripts/bilevel.sbatch
+			sbatch --export=FOLDER=$FOLDER,SOLVER=$SOLVER,SEED=$SEED,YDIM=$YDIM,NCON=$NCON,ITER=$ITER,EPS=$EPS scripts/bilevel.sbatch
 		done
 	done
 done
--- scripts/exp2.sh
+++ scripts/exp2.sh
@@ -2,6 +2,7 @@
 
 ITER=10
 FOLDER=exp2
+EPS=0.01
 
 for SEED in {1..5}
 do
@@ -10,7 +11,7 @@ do
 		NCON=$((YDIM/5))
 		for SOLVER in cvxpylayer ffo
 		do
-			sbatch --export=FOLDER=$FOLDER,SOLVER=$SOLVER,SEED=$SEED,YDIM=$YDIM,NCON=$NCON,ITER=$ITER scripts/bilevel.sbatch
+			sbatch --export=FOLDER=$FOLDER,SOLVER=$SOLVER,SEED=$SEED,YDIM=$YDIM,NCON=$NCON,ITER=$ITER,EPS=$EPS scripts/bilevel.sbatch
 		done
 	done
 done
