b2de3f807c83204058a80103010d984099e0b5a9
Kai Wang <kwang692@login-phoenix-slurm-2.pace.gatech.edu>
2024-05-10 23:23:29 -0400

    added experimental results

--- scripts/exp1.sh
+++ scripts/exp1.sh
@@ -2,12 +2,12 @@ FOLDER=exp1
 EPS=0.01
 ITER=1000
 
-for SEED in 1  # {1..5}
+for SEED in {1..5}
 do
-	for YDIM in 500 600 700 800 900 1000 # 5 10 20 50 100 200 300 400 500 600 700 800 900 1000
+	for YDIM in  5 10 20 50 100 200 500 800 1000
 	do
 		NCON=$((YDIM/5))
-		for SOLVER in ffo # cvxpylayer ffo
+		for SOLVER in cvxpylayer ffo
 		do
 			sbatch --export=FOLDER=$FOLDER,SOLVER=$SOLVER,SEED=$SEED,YDIM=$YDIM,NCON=$NCON,ITER=$ITER,EPS=$EPS scripts/bilevel.sbatch
 		done
