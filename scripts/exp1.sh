d4a312ef6b669e7146e6ae78e9af499f978c3a13
guaguakai <kwang692@gatech.edu>
2024-10-19 16:55:55 -0700

    updated exp1.sh

--- scripts/exp1.sh
+++ scripts/exp1.sh
@@ -4,7 +4,7 @@ ITER=1000
 
 for SEED in {1..10}
 do
-	for YDIM in  5 10 20 50 100 200 500 800 1000
+	for YDIM in  5 10 20 50 100 200 500
 	do
 		NCON=$((YDIM/5))
 		for SOLVER in ffo_complex # cvxpylayer ffo
